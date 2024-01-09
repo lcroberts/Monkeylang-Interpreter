2426b938f17fd6d8a1642d1e60d0c1607842cb89
Logan Roberts <logancroberts@outlook.com>
2024-01-09 17:53:38 -0500

    Fix test error message

--- MyCode/src/monkey/lexer/lexer_test.go
+++ MyCode/src/monkey/lexer/lexer_test.go
@@ -66,6 +66,10 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
