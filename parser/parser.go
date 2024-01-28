7b86d409560cee78be7bc46e58200728387be1b6
Logan Roberts <logancroberts@outlook.com>
2024-01-28 10:31:14 -0500

    feat: finish infix expression parsing. Up to section 2.7 (page 69)

--- parser/parser.go
+++ parser/parser.go
@@ -167,12 +167,21 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
-
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
+
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+		p.nextToken()
+
+		leftExp = infix(leftExp)
+	}
 
 	return leftExp
 }
